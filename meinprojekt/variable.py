5a23ad7f1f5f97b6132ee9165d4fa196a9c025a1
Martin <martin@example.de>
2025-05-23 11:28:59 +0200

    Aufgabe 3.1.5

---
+++ meinprojekt/variable.py
@@ -0,0 +1,28 @@
+größe = "Blau"
+farbe = "Groß"
+anzahlTüren = "3 Türen"
+kraftstoff = "Diesel"
+fahrzeugtyp = "LKW"
+hersteller = "BMW"
+baujahr = "1985"
+getriebe = "Manuell"
+reifen = "Winterreifen"
+Unfallfrei = "Nein"
+
+print("Fahrzeugdetails:")
+print("Farbe:", farbe)
+print("Größe:", größe)
+print("Türen:", anzahlTüren)
+print("Kraftstoff:", kraftstoff)
+print("Fahrzeugtyp:", fahrzeugtyp)
+print("Hersteller:", hersteller)
+print("Baujahr:", baujahr)
+print("Getriebe:", getriebe)
+print("Reifen:", reifen)
+print("Unfallfrei:", Unfallfrei)
+
+größe, farbe = farbe, größe ## Tausch der variable
+
+print("Tausche 'farbe' mit 'größe':")
+print("Größe:", größe)
+print("Farbe:", farbe)
